7c771cc76008bc61d03dc400da6c957333b165f6
Tolga Karabulut <speak.abonelikler@gmail.com>
2019-12-27 23:48:57 +0300

    Route added.

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'book'], function () {
+    Route::post('create', 'BookController@create');
+    Route::get('list', 'BookController@list');
+    Route::delete('delete', 'BookController@delete');
+    Route::put('update', 'BookController@update');
+});
